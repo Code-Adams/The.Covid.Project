5c23b5092c915c97a53d2d80ed63c0d77dfc433d
sakshm bhat <sakshm2000@gmail.com>
2020-11-28 17:47:34 +0530

    requestAdapter Main Activity improved

--- app/src/main/java/com/sakshmbhat/thecovidproject/MainActivity.java
+++ app/src/main/java/com/sakshmbhat/thecovidproject/MainActivity.java
@@ -39,39 +39,34 @@ import java.util.List;
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends AppCompatActivity {
+    //Don't modify their content once initial initialization is done
+    private FloatingActionButton fAB;
+    private FirebaseAuth mAuth;
+    private FirebaseUser currentUser;
+    private int flag=0;
 
-     private FirebaseAuth mAuth;
-     private FirebaseUser mCurrentUser;
-     private DatabaseReference databaseReference;
-    private FloatingActionButton mfab;
-    private TextView fakeTextForUid,getFakeTextForPhonenumber;
-    private List<RequestData> requestDataList;
-    RecyclerView mRecyclerView;
-    DatabaseReference rootref;
-    RecyclerView recycler;
-    Query queryCurrentDepartment;
-    String currentUserDept="";
+    private DatabaseReference rootReference;
+
+    private RecyclerView requestRecycler;
+    private ArrayList<RequestData> requestDataList;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fakeTextForUid=findViewById(R.id.fakeTextUid);
-        getFakeTextForPhonenumber=findViewById(R.id.fakeTextPhoneNumber);
-        mAuth= FirebaseAuth.getInstance();
-        mCurrentUser= mAuth.getCurrentUser();
-        recycler=findViewById(R.id.requestRecycler);
-        rootref=FirebaseDatabase.getInstance().getReference();
-
-        mfab=findViewById(R.id.myfab);
-
-        if(mCurrentUser!=null)
+        if(authenticationAndRegistrationCheck())
         {
-            setAdapterForRequests();
+            initialiseData();
+            setAdapter();
         }
 
 
-        mfab.setOnClickListener(new View.OnClickListener() {
+
+
+
+
+        fAB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent Aintent = new Intent(MainActivity.this,AddRequest.class);
@@ -83,45 +78,46 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    private void setAdapterForRequests() {
-
-
-       // queryCurrentDepartment=rootref.child("Requests").orderByChild("Department_Name").equalTo(currentUserDept);
-        DatabaseReference dbRef= FirebaseDatabase.getInstance().getReference().child("Users").child(mCurrentUser.getUid());
-
-        dbRef.addValueEventListener(new ValueEventListener() {
+    private void setAdapter() {
+         requestDataList = new ArrayList<>();
+        rootReference.child("Users").child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                if(snapshot.exists())
-                {
-                    currentUserDept= snapshot.child("department").getValue().toString();
-                    DatabaseReference requestReference= FirebaseDatabase.getInstance().getReference().child("Requests");
-                    requestReference.addValueEventListener(new ValueEventListener() {
+                if(snapshot.exists()){
+
+                    rootReference.child("Requests").addValueEventListener(new ValueEventListener() {
                         @Override
-                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        public void onDataChange(@NonNull DataSnapshot Snapshot) {
 
-                            requestDataList= new ArrayList<>();
+                            if(Snapshot.exists()){
 
-                            if(snapshot.exists())
-                            {
+                                for(DataSnapshot dataSnapshot : Snapshot.getChildren()){
 
-                                for(DataSnapshot dataSnapshot :snapshot.getChildren())
-                                {
-                                    if(dataSnapshot.child("Department_Name").getValue().toString().equals(currentUserDept)&&!dataSnapshot.child("Generator").getValue().toString().equals(mCurrentUser.getUid()))
-                                    {
-                                               RequestData requestData= dataSnapshot.getValue(RequestData.class);
-                                               requestDataList.add(requestData);
+                                    if(dataSnapshot.child("Department_Name").getValue().toString().equals(snapshot.child("department").getValue().toString())){
+
+                                        if(!dataSnapshot.child("Generator").getValue().toString().equals(currentUser.getUid())){
+
+                                            RequestData requestData = dataSnapshot.getValue(RequestData.class);
+                                            requestDataList.add(requestData);
+
+
+
+
+                                        }
+
                                     }
+                                    RequestData requestData = dataSnapshot.getValue(RequestData.class);
+                                    requestDataList.add(requestData);
 
                                 }
-
-                                recycler.setHasFixedSize(true);
-                                recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
-                                requestAdapter adapter = new requestAdapter(requestDataList,MainActivity.this);
-                                recycler.setAdapter(adapter);
+                                              requestRecycler.setHasFixedSize(true);
+                                              requestRecycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+                                              requestAdapter adapter = new requestAdapter(requestDataList, MainActivity.this);
+                                              requestRecycler.setAdapter(adapter);
 
 
                             }
+
                         }
 
                         @Override
@@ -129,7 +125,6 @@ public class MainActivity extends AppCompatActivity {
 
                         }
                     });
-
 
                 }
             }
@@ -142,51 +137,198 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void initialiseData() {
 
-    //See if user is logged-in in on START METHOD if not then login first
-    @Override
-    protected void onStart() {
-        super.onStart();
-
-        checkRegistrationAndAuthentication();
-
-
+        rootReference= FirebaseDatabase.getInstance().getReference();
+        requestRecycler= findViewById(R.id.requestRecycler);
+        fAB=findViewById(R.id.myfab);
 
     }
 
-         private void checkRegistrationAndAuthentication() {
+    Boolean authenticationAndRegistrationCheck()
+     {
+         mAuth =FirebaseAuth.getInstance();
+         currentUser= mAuth.getCurrentUser();
 
-        if(mCurrentUser==null)
-        {
+         if (currentUser == null) {
             //Send user to phone login activity
-            Intent intent= new Intent(MainActivity.this, PhoneNumberActivity.class);
-            //Don't allow user to come back on pressing back button by clearing task and top
+            Intent intent = new Intent(MainActivity.this, PhoneNumberActivity.class);
+
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
             startActivity(intent);
             finish();
-        }
-        else {
-            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
-            databaseReference = FirebaseDatabase.getInstance().getReference();
-            databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-                    if (!snapshot.hasChild(uid)) {
-                        Intent intent = new Intent(MainActivity.this, RegistrationAvtivity.class);
-                        startActivity(intent);
-                    }
+            return false;
+        }else if(currentUser!=null){
+             FirebaseDatabase.getInstance().getReference().child("Users").addValueEventListener(new ValueEventListener() {
+                 @Override
+                 public void onDataChange(@NonNull DataSnapshot snapshot) {
 
-                }
+                         if(snapshot.exists()&& !snapshot.hasChild(currentUser.getUid())){
 
-                @Override
-                public void onCancelled(@NonNull DatabaseError error) {
+                             Intent Rintent = new Intent(MainActivity.this, RegistrationAvtivity.class);
+                             Rintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                             Rintent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                             flag=1;
+                             startActivity(Rintent);
+                             finish();
 
-                }
-            });
 
-        }
+                         }
+                 }
 
-    }
+                 @Override
+                 public void onCancelled(@NonNull DatabaseError error) {
 
+                 }
+             });
+             if(flag==1)
+             return false;
+             else
+                 return true;
+         }
+         else{
+             return  true;
+         }
+     }
+
+//        private void setAdapterForRequests () {
+//
+//
+//        // queryCurrentDepartment=rootref.child("Requests").orderByChild("Department_Name").equalTo(currentUserDept);
+//        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Users/" + mCurrentUser.getUid() + "/");
+//        DatabaseReference check = FirebaseDatabase.getInstance().getReference().child("Users");
+//        check.child(mCurrentUser.getUid()).addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                //Toast.makeText(MainActivity.this, snapshot.child("phoneNumber").getValue().toString(), Toast.LENGTH_SHORT).show();
+//                String tempCurrentUserDept = snapshot.child("department").getValue().toString();
+//                currentDeptSaver(tempCurrentUserDept);
+////                DatabaseReference requestReference= FirebaseDatabase.getInstance().getReference("Requests/");
+////               //Toast.makeText(MainActivity.this, requestReference.toString(), Toast.LENGTH_SHORT).show();
+////                requestReference.addValueEventListener(new ValueEventListener() {
+////                    @Override
+////                    public void onDataChange(@NonNull DataSnapshot Snapshot) {
+////                       // Toast.makeText(MainActivity.this, Snapshot.child("phoneNumber").getValue().toString(), Toast.LENGTH_SHORT).show();
+////                        for(DataSnapshot dataSnapshot :Snapshot.getChildren())
+////                        {
+////                            Toast.makeText(MainActivity.this, Snapshot.child("Item_Name").getValue().toString(), Toast.LENGTH_SHORT).show();
+////                            if(dataSnapshot.child("Department_Name").getValue().toString().equals(currentUserDept)&&!dataSnapshot.child("Generator").getValue().toString().equals(mCurrentUser.getUid()))
+////                            {
+////                                RequestData requestData= dataSnapshot.getValue(RequestData.class);
+////                                requestDataList.add(requestData);
+////                            }
+////
+////                        }
+////
+////                        recycler.setHasFixedSize(true);
+////                        recycler.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+////                        requestAdapter adapter = new requestAdapter(requestDataList,MainActivity.this);
+////                        recycler.setAdapter(adapter);
+////                    }
+////
+////                    @Override
+////                    public void onCancelled(@NonNull DatabaseError error) {
+////
+////                    }
+////                });
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+//
+//        DatabaseReference newRequestReference = FirebaseDatabase.getInstance().getReference("Requests");
+//
+//        newRequestReference.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot Snapshot) {
+//                Toast.makeText(MainActivity.this, currentUserDept, Toast.LENGTH_SHORT).show();
+//                requestDataList = new ArrayList<>();
+//
+//                if (Snapshot.exists()) {
+//                    for (DataSnapshot dataSnapshot : Snapshot.getChildren()) {
+//                        Toast.makeText(MainActivity.this, dataSnapshot.child("Department_Name").getValue().toString() + "Thisone", Toast.LENGTH_SHORT).show();
+//                        if (dataSnapshot.child("Department_Name").getValue().toString().equals(currentUserDept))//&&!dataSnapshot.child("Generator").getValue().toString().equals(mCurrentUser.getUid()))
+//                        {
+//
+//                        }
+//
+//                        RequestData requestData = dataSnapshot.getValue(RequestData.class);
+//                        requestDataList.add(requestData);
+//
+//                    }
+//
+//                    recycler.setHasFixedSize(true);
+//                    recycler.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
+//                    requestAdapter adapter = new requestAdapter(requestDataList, MainActivity.this);
+//                    recycler.setAdapter(adapter);
+//
+//
+//                } else {
+//                    Toast.makeText(MainActivity.this, currentUserDept, Toast.LENGTH_SHORT).show();
+//                }
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_SHORT).show();
+//
+//            }
+//        });
+//
+//
+//    }
+//
+//        private void currentDeptSaver (String tempCurrentUserDept){
+//
+//        currentUserDept = tempCurrentUserDept;
+//        // Toast.makeText(MainActivity.this,currentUserDept, Toast.LENGTH_SHORT).show();
+//    }
+//
+//        //See if user is logged-in in on START METHOD if not then login first
+//        @Override
+//        protected void onStart () {
+//        super.onStart();
+//
+//        checkRegistrationAndAuthentication();
+//
+//    }
+//
+//        private void checkRegistrationAndAuthentication () {
+//
+//        if (mCurrentUser == null) {
+//            //Send user to phone login activity
+//            Intent intent = new Intent(MainActivity.this, PhoneNumberActivity.class);
+//            //Don't allow user to come back on pressing back button by clearing task and top
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+//            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+//            startActivity(intent);
+//            finish();
+//        } else {
+//            String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+//            databaseReference = FirebaseDatabase.getInstance().getReference();
+//            databaseReference.child("Users").addListenerForSingleValueEvent(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                    if (!snapshot.hasChild(uid)) {
+//                        Intent intent = new Intent(MainActivity.this, RegistrationAvtivity.class);
+//                        startActivity(intent);
+//                    }
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(@NonNull DatabaseError error) {
+//
+//                }
+//            });
+//
+//        }
+//
+//    }
+//    }
 }
--- app/src/main/java/com/sakshmbhat/thecovidproject/requestAdapter.java
+++ app/src/main/java/com/sakshmbhat/thecovidproject/requestAdapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -69,10 +70,11 @@ public class requestAdapter extends  RecyclerView.Adapter<requestAdapter.request
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(Intent.ACTION_CALL);
-                intent.setData(Uri.parse("tel:"+holder.phoneNumber.getText().toString().trim()));
+                Toast.makeText(context, holder.phoneNumber.getText().toString(), Toast.LENGTH_SHORT).show();
+                Intent Cintent = new Intent(Intent.ACTION_CALL);
+                Cintent.setData(Uri.parse("tel:"+holder.phoneNumber.getText().toString().trim()));
 
-                context.startActivity(intent);
+                context.startActivity(Cintent);
 
             }
         });
